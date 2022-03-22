31ed07ffe98bcaed7393dd2f57ecad657fb40858
Mithz <mithileshbele2002@gmail.com>
2022-03-22 13:13:22 +0530

    commit

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,17 @@
 function Flights() {
 
+    function calculateNumberOfFlights(passengers,capacity){
+        let flights;
+
+        if(passengers % capacity == 0){
+            flights = passengers/capacity;
+        }else{
+            flights = Math.floor(passengers/capacity) + 1;
+        }
+        return flights;
+    }
+    return {calculateNumberOfFlights}
+
 }
+module.exports = Flights();
 
