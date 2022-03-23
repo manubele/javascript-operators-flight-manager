aff0741e283c7d6fc4d5c1ca556488c249315736
Mithz <mithileshbele2002@gmail.com>
2022-03-23 19:47:58 +0530

    Completed the project

--- logic/flights.js
+++ logic/flights.js
@@ -1,17 +1,25 @@
 function Flights() {
-
-    function calculateNumberOfFlights(passengers,capacity){
+    function calculateNumberOfFlights(passengers, capacity) {
         let flights;
 
-        if(passengers % capacity == 0){
+        if ((passengers < 0) || (!Number.isInteger(Number(passengers)))) {
+            throw new Error("The number of passengers must be a positive integer value")
+        }
+
+        if ((capacity < 0) || (!Number.isInteger(Number(capacity)))) {
+            throw new Error("The capacity of the flight must be a positive integer value")
+        }
+
+        if (passengers % capacity == 0) {
             flights = passengers/capacity;
-        }else{
+        } else {
             flights = Math.floor(passengers/capacity) + 1;
         }
         return flights;
     }
-    return {calculateNumberOfFlights}
+
+    return {calculateNumberOfFlights};
 
 }
-module.exports = Flights();
 
+module.exports = Flights();
