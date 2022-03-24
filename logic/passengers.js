e48766faab0a9e6807c256599285afdc48ba3388
Mithz <mithileshbele2002@gmail.com>
2022-03-24 22:20:34 +0530

    Completed the project

--- logic/passengers.js
+++ logic/passengers.js
@@ -1,4 +1,18 @@
 function Passengers() {
 
+    function checkFlightCapacity(flightCapacity, passengersArray) {
+        let passengersNumber = 0;
+        let passengers;
+        for(passengers of passengersArray) {
+            passengersNumber += passengers;
+        }
+        if (passengersNumber > flightCapacity) {
+            throw new Error("Flight capacity (" + flightCapacity + ") exceeded. You have " + passengersNumber + " passengers.");
+        }
+        return passengersNumber;
+    }
+     return{checkFlightCapacity};
+     
 }
-
+     
+module.exports = Passengers(); 
