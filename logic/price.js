d29ed1887b50b7c55c1bb5ee0ce5fa9a68c7a979
Mithz <mithileshbele2002@gmail.com>
2022-03-24 22:39:47 +0530

    Completed the project

--- logic/price.js
+++ logic/price.js
@@ -1,4 +1,15 @@
 function Prices() {
 
+    function calculatedFinalPrice(basePrice, percentPassengerType, percentFlightType){
+        let finalPrice = basePrice;
+
+        finalPrice *= 1+percentPassengerType/100;
+        finalPrice *= 1+percentFlightType/100;
+
+        return finalPrice.toFixed(2);
+    }
+
+    return {calculatedFinalPrice};
 }
 
+module.exports = Prices();
